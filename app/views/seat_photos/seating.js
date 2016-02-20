14a75faece527b310f2dad18146a7718a082d335
dianacamacho <dcam1901@yahoo.com>
2016-02-19 23:52:11 -0600

    seat color change on seat chart seats first row, still in progress

--- app/views/seat_photos/seating.js
+++ app/views/seat_photos/seating.js
@@ -1,56 +1,49 @@
 var canvas = document.getElementById("seating");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "#ff0000";
-ctx.fillRect(0,0,150,75);
+ctx.rect(0,0,500,500); // 
+ctx.rect(10,10,480,100); // stage
+ctx.rect(10,120,230,200); // left section
+ctx.rect(260,120,230,200); // right section
+ctx.stroke();
 
+var moveX = 34;
+var x = 27;
+var y = 150;
+var radius = 7;
+var startAngle = 0;
+var endAngle = Math.PI*2;
 
-var canvas = document.getElementById("seating");
-if (canvas.getContext){
-  var ctx = canvas.getContext('2d');
+ctx.fillStyle = "#5e80ae";
+ctx.beginPath();
+ctx.moveTo(moveX,y)
+ctx.arc(x, y, radius, startAngle, endAngle);
+ctx.fill();
 
-  ctx.fillStyle = "#5e80ae";
-  ctx.fillRect(0,0,500,500); // 
-  ctx.strokeRect(10,10,480,100); // stage
-  ctx.strokeRect(10,120,230,200); // left section
-  ctx.strokeRect(260,120,230,200); // right section
-  
-  var moveX = 34;
-  var x = 27;
-  var y = 150;
-  var radius = 7;
-  var startAngle = 0;
-  var endAngle = Math.PI*2;
+for(var i = 0; i < 8; i++) {
 
-  ctx.moveTo(moveX,y)
+  moveX += 24;
+  x += 24;
+
+  ctx.moveTo(moveX, y)
   ctx.arc(x, y, radius, startAngle, endAngle);
-
-  for(var i = 0; i < 8; i++) {
-  
-    moveX += 24;
-    x += 24;
-
-    ctx.moveTo(moveX, y)
-    ctx.arc(x, y, radius, startAngle, endAngle);
-  }
-
-  var moveX = 284;
-  var x = 277;
-
-  ctx.moveTo(moveX,y)
-  ctx.arc(x, y, radius, startAngle, endAngle);
-
-  for(var i = 0; i < 8; i++) {
-  
-    moveX += 24;
-    x += 24;
-   
-    ctx.moveTo(moveX, y)
-    ctx.arc(x, y, radius, startAngle, endAngle);
-  }
-
-  ctx.stroke();
-
+  ctx.fill();
 }
 
+var moveX = 284;
+var x = 277;
 
+ctx.moveTo(moveX,y)
+ctx.arc(x, y, radius, startAngle, endAngle);
+
+for(var i = 0; i < 8; i++) {
+
+  moveX += 24;
+  x += 24;
+ 
+  ctx.moveTo(moveX, y)
+  ctx.arc(x, y, radius, startAngle, endAngle);
+  ctx.fill();
+}
+
+ctx.stroke();
