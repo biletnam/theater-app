83d626f83f949984b3a5658759190693fe2401f8
dianacamacho <dcam1901@yahoo.com>
2016-03-20 23:43:20 -0500

    added sort and reverse sort to venues seating chart page. still need to add columns on table for links to view seat photos and add seat photos

--- app/assets/javascripts/seats_ctrl.js
+++ app/assets/javascripts/seats_ctrl.js
@@ -10,6 +10,10 @@
     });
   };
 
+  $scope.sort = function(keyname){
+    $scope.sortKey = keyname;   //set the sortKey to the param passed
+    $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+  };
 
 
   window.scope = $scope;
