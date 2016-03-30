c191bc792c172089a9f78df029e8b5171b771d00
dianacamacho <dcam1901@yahoo.com>
2016-03-29 22:36:56 -0500

    api edit

--- app/controllers/api/v1/gp_restaurants_controller.rb
+++ app/controllers/api/v1/gp_restaurants_controller.rb
@@ -4,7 +4,6 @@ def index
   @venue = Venue.find(params[:id])
   @gp_api_restaurants_response = Unirest.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@venue.latitude},#{@venue.longitude}&rankby=distance&type=restaurant&key=#{ENV["google_places_key"]}").body
    
-
   @price_level = "$"
 end
  
