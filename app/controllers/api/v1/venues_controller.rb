ae0b196f7bb723ef8e038d941a8e694987e62cf1
Diana Camacho <dcam1901@yahoo.com>
2016-05-30 11:09:45 -0500

    Update venues_controller.rb

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::VenuesController < ApplicationController
 
-def index
-  @venues = Venue.all
-end
-
-def show
-  @venue = Venue.find(params[:id])
-end
+  def index
+    @venues = Venue.all
+  end
+  
+  def show
+    @venue = Venue.find(params[:id])
+  end
 
 end
